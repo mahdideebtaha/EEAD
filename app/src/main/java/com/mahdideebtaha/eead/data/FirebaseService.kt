92f2c14c17137b055bb29835a00ad42404f68021
mahdidot22 <mahditaha1011@gamil.com>
2025-09-27 13:28:18 +0300

    firebase connection

--- app/src/main/java/com/mahdideebtaha/eead/data/FirebaseService.kt
+++ app/src/main/java/com/mahdideebtaha/eead/data/FirebaseService.kt
@@ -1,7 +1,14 @@
 package com.mahdideebtaha.eead.data
 
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.firestore
+import com.mahdideebtaha.eead.model.BenchmarkResult
+
 object FirebaseService {
-    private val db = FirebaseFirestore.getInstance()
+
+    private val db: FirebaseFirestore
+        get() = Firebase.firestore
 
     fun uploadBenchmark(result: BenchmarkResult, onSuccess: () -> Unit, onFailure: (Exception) -> Unit) {
         db.collection("benchmarks")
